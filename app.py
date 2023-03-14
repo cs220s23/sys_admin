cf9d9cc2d64bbc9b36a30f0fb0c1e09274ebc552
Ben Coleman <colemanb@moravian.edu>
2023-03-14 07:20:51 -0400

    basic app

---
+++ app.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def greet():
+    result = {'foo': 'bar',
+              'value': 42}
+
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='0.0.0.0')
